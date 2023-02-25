f3b5aa36deb4c1ec02bc95a6b81768d0e6aaddd2
Sebiten <sebdevspace@gmail.com>
2023-02-25 13:32:22 -0300

    dona

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 // next.config.js
+
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://www.omdbapi.com/:path*',
-      },
-    ];
-  },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        port: '',
-        pathname: '/images/**',
-      },
-    ],
+    domains: ['www.omdbapi.com', 'm.media-amazon.com'],
   },
-}
+};
